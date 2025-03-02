b122db7b5b0d85bbe7089015d00957278b39ee1e
Wikonire <ni.widmer@bluewin.ch>
2025-03-02 11:43:04 +0100

    Add ProjectDetailComponent routing and clean dependencies
    Introduced a new route for ProjectDetailComponent and updated the app navigation to include it. Removed unused dependencies, including @worktile/gantt and html2canvas, simplifying the package structure and improving maintainability.

--- front-manager/src/app/app.routes.ts
+++ front-manager/src/app/app.routes.ts
@@ -3,10 +3,12 @@ import {HomeComponent} from './home/home.component';
 import {ProjectsComponent} from './projects/projects.component';
 import {UserAccountComponent} from './user-account/user-account.component';
 import {UserManagementComponent} from './user-management/user-management.component';
+import {ProjectDetailComponent} from './project-detail/project-detail.component';
 
 export const routes: Routes = [
   {title: 'Home - ProjectManager', path: 'home', component: HomeComponent},
   {title: 'Projects - ProjectManager', path: 'project-overview', component: ProjectsComponent},
   {title: 'Account - ProjectManager', path: 'account', component: UserAccountComponent},
   {title: 'User Management - ProjectManager', path: 'user-management', component: UserManagementComponent},
+  {title: 'Project-Details - ProjectManager', path: 'project-details', component: ProjectDetailComponent},
 ];
