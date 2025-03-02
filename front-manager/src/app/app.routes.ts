a223e7997cba423f4388a300f10ffd0794048da7
Wikonire <ni.widmer@bluewin.ch>
2025-03-02 02:31:01 +0100

    Add User Management, User Account, and Projects components
    Introduce new components for managing user accounts and projects, along with their routing and basic templates. Add unit tests for all new components to ensure proper initialization and functionality. Update the router configuration to include paths for these components.

--- front-manager/src/app/app.routes.ts
+++ front-manager/src/app/app.routes.ts
@@ -1,6 +1,12 @@
 import { Routes } from '@angular/router';
 import {HomeComponent} from './home/home.component';
+import {ProjectsComponent} from './projects/projects.component';
+import {UserAccountComponent} from './user-account/user-account.component';
+import {UserManagementComponent} from './user-management/user-management.component';
 
 export const routes: Routes = [
   {title: 'Home - ProjectManager', path: 'home', component: HomeComponent},
+  {title: 'Projects - ProjectManager', path: 'project-overview', component: ProjectsComponent},
+  {title: 'Account - ProjectManager', path: 'account', component: UserAccountComponent},
+  {title: 'User Management - ProjectManager', path: 'user-management', component: UserManagementComponent},
 ];
