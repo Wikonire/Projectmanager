871b3cdc1688c4064056e666378687c13a41b437
Wikonire <ni.widmer@bluewin.ch>
2025-03-02 02:30:41 +0100

    Add basic layout with Angular Material components
    Introduced a new toolbar and navigation menu using Angular Material components (Toolbar, Button, Menu, Icon). Updated app structure and styles for header, main content, and footer to provide a foundational layout for the application.

--- front-manager/src/app/app.component.ts
+++ front-manager/src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {RouterLink, RouterLinkActive, RouterOutlet} from '@angular/router';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
+import { MatIconModule} from '@angular/material/icon';
+import {MatMenuModule} from '@angular/material/menu';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatMenuModule, MatToolbarModule, MatButtonModule, MatIconModule, RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
