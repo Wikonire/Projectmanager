89e429e87a73aa2e625f89476b11c1b446805477
Wikonire <ni.widmer@bluewin.ch>
2025-03-02 02:38:48 +0100

    Integrate Gantt chart into project-detail component.
    Added NgxGantt components to the project-detail component to display tasks in a Gantt chart format. Introduced mock Gantt items and updated the template to include a table with task details. This enhances visual task management within the application.

--- front-manager/src/app/project-detail/project-detail.component.ts
+++ front-manager/src/app/project-detail/project-detail.component.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
+import {GanttItem, NgxGanttComponent, NgxGanttTableColumnComponent, NgxGanttTableComponent} from '@worktile/gantt';
 
 @Component({
   selector: 'app-project-detail',
-  imports: [],
+  imports: [
+    NgxGanttTableColumnComponent,
+    NgxGanttTableComponent,
+    NgxGanttComponent
+  ],
   templateUrl: './project-detail.component.html',
   styleUrl: './project-detail.component.scss'
 })
 export class ProjectDetailComponent {
-
+  items: GanttItem[] = [
+    { id: '000000', title: 'Task 0', start: 1627729997, end: 1628421197 },
+    { id: '000001', title: 'Task 1', start: 1617361997, end: 1625483597 }
+  ];
 }
