566fa85872e262212a8852f9162229309bfc47c6
Wikonire <ni.widmer@bluewin.ch>
2025-03-17 16:57:12 +0100

    Refactor AuthController imports and formatting.
    Simplified imports by removing unused ones and reformatted the code to improve readability and consistency. Cleaned up unnecessary comments and adjusted constructor style for better clarity.

--- back-manager/src/auth/auth.controller.ts
+++ back-manager/src/auth/auth.controller.ts
@@ -1,15 +1,14 @@
-import {Body, Controller, Get, Param, Post, Request, UseGuards} from '@nestjs/common';
-import { AuthService } from './auth.service';
-import { AuthGuard } from '@nestjs/passport';
+import {Body, Controller, Post} from '@nestjs/common';
+import {AuthService} from './auth.service';
 import {LoginDto} from '../dtos/login.dto';
 
 // @UseGuards(AuthGuard('local'))
 @Controller('auth')
 export class AuthController {
-    constructor(private authService: AuthService) {}
+    constructor(private authService: AuthService) {
+    }
 
-    @Post('login')
-    async login(@Body() loginDto: LoginDto) {
+    @Post('login') async login(@Body() loginDto: LoginDto) {
         return this.authService.login(loginDto);
     }
 }
